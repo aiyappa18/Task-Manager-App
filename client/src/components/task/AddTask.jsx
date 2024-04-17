035402ba85c27f0e9fd7ae779625223921fc65b0
aiyappa <mnea2001@gmail.com>
2024-04-17 23:34:31 +0530

    integrated with firebase to store images of assets

--- client/src/components/task/AddTask.jsx
+++ client/src/components/task/AddTask.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import ModalWrapper from "../ModelWrapper";
 import { Dialog } from "@headlessui/react";
-
+import {toast} from "sonner";
 import { useForm } from "react-hook-form";
 import UserList from "./UserList";
 import SelectList from "../SelectList";
 import { BiImages } from "react-icons/bi";
 import Button from "../Button";
 import TextBox from "../TextBox.jsx";
+import {getStorage,ref,getDownloadURL,uploadBytesResumable} from "firebase/storage";
+import {app} from "../../utils/firebase.js";
+import { useCreateTaskMutation, useUpdateTaskMutation } from "../../redux/slices/api/taskApiSlice.js";
+import { dateFormatter } from "../../utils/index.js";
 
 const LISTS = ["TODO", "IN PROGRESS", "COMPLETED"];
 const PRIORIRY = ["HIGH", "MEDIUM", "NORMAL", "LOW"];
 
 const uploadedFileURLs = [];
 
-const AddTask = ({ open, setOpen }) => {
-  const task = "";
+const AddTask = ({ open, setOpen ,task}) => {
+  const defaultValues={
+    title:task?.title || "",
+    date:dateFormatter(task?.date || new Date()),
+    team:[],
+    stage:"",
+    priority:"",
+    assets:[]
+  };
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({defaultValues});
   const [team, setTeam] = useState(task?.team || []);
   const [stage, setStage] = useState(task?.stage?.toUpperCase() || LISTS[0]);
   const [priority, setPriority] = useState(
@@ -30,12 +41,81 @@ const AddTask = ({ open, setOpen }) => {
   const [assets, setAssets] = useState([]);
   const [uploading, setUploading] = useState(false);
 
-  const submitHandler = () => {};
+  const [createTask,{isLoading}]=useCreateTaskMutation();
+  const [updateTask,{isLoading:isUpdating}]=useUpdateTaskMutation();
+  const URLS=task?.assets ?[...task.assets]:[];
+  
+  const submitHandler = async(data) => {
+    for(const file of assets)
+    {
+      setUploading(true);
+      try{
+        await uploadFile(file);
+      }
+      catch(error)
+      {
+        console.log(error.message);
+        return;
+      }
+      finally{
+        setUploading(false);
+      }
+    }
+    try{
+      const newData={
+        ...data,
+        assets:[...URLS,...uploadedFileURLs],
+        team,
+        stage,
+        priority
+      };
+      const res=task?._id
+      ?await updateTask({...newData,_id:task._id}).unwrap()
+      :await createTask(newData).unwrap();
+
+      toast.success(res.message);
+
+      setTimeout(()=>{
+        setOpen(false);
+      },500)
+    }
+    catch(err)
+    {
+      console.log(err);
+      toast.error(err?.data.message || err.error);
+    }
+  };
 
   const handleSelect = (e) => {
     setAssets(e.target.files);
   };
 
+  const uploadFile=async(file)=>{
+    const storage=getStorage(app);
+    const name=new Date().getTime()+ file.name;
+    const storageRef=ref(storage, name);
+
+    const uploadTask=uploadBytesResumable(storageRef, file);
+
+    return new Promise((resolve,reject)=>{
+      uploadTask.on(
+        "state_changed",
+        (snapshot)=>{
+          console.log("Uploading..");
+        },
+        (error)=>{
+          reject(error);
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL)=>{
+            uploadedFileURLs.push(downloadURL);
+            resolve();
+          })
+        }
+      )
+    })
+  }
   return (
     <>
       <ModalWrapper open={open} setOpen={setOpen}>
